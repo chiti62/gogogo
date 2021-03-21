a5b5d27669013ae469e7a63bedbb493521df0aa2
ChinKai Ting <ckting@ChinKais-MacBook-Pro.local>
2021-03-21 22:42:27 +0800

    finish error

---
+++ ch2/global/setting.go
@@ -0,0 +1,12 @@
+package global
+
+import "github.com/chiti62/gogogo/ch2/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettings
+	AppSetting      *setting.AppSettings
+	EmailSetting    *setting.EmailSettings
+	JWTSetting      *setting.JWTSettings
+	DatabaseSetting *setting.DatabaseSettings
+	// Logger          *logger.Logger
+)
--- ch2/main.go
+++ ch2/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/chiti62/gogogo/ch2/global"
 	"github.com/chiti62/gogogo/ch2/internal/routers"
+	"github.com/chiti62/gogogo/ch2/pkg/setting"
+	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err) //os.Exit, no stacktrace
+	}
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
 		Addr:           ":8080",
@@ -17,4 +30,28 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%v, %v, %v", global.ServerSetting, global.AppSetting, global.DatabaseSetting)
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
